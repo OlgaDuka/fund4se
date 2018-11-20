b9980b14a8a4b69d40f68590299e591c67f03c6c
Olga Duka <olgaduk@mail.ru>
2018-11-20 23:33:30 +0600

    Размечает Инвесторов и Стартаперов

--- js/scripts/header-nav.js
+++ js/scripts/header-nav.js
@@ -1,15 +1,17 @@
 'use strict';
 
 (function () {
-  var menu = document.querySelector('.menu');
-  var button = document.querySelector('.header__burger');
-  var contacts = document.querySelector('.header__contacts');
+  var panel = document.querySelector('.header');
+  var menu = panel.querySelector('.menu');
+  var button = panel.querySelector('.header__burger');
+  var contacts = panel.querySelector('.header__contacts');
 
   // Закрываем меню, если JS работает
   menu.classList.remove('menu--nojs');
 
   var toggleMenu = function (evt) {
     evt.preventDefault();
+    panel.classList.toggle('header--opened');
     menu.classList.toggle('menu--opened');
     button.classList.toggle('header__burger--opened');
     contacts.classList.toggle('header__contacts--opened');
@@ -24,6 +26,7 @@
   var setListeners = function () {
     setMenuListener();
     if (window.innerWidth > 767) {
+      panel.classList.remove('header--opened');
       menu.classList.remove('menu--opened');
     }
   }
