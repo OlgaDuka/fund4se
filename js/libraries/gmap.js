6162f8b801b3f46428bb078b4ebf69ac60ac86a6
Olga Duka <olgaduk@mail.ru>
2018-11-28 10:33:13 +0600

    Подключает карту гугл, меняет ключ API

--- js/libraries/gmap.js
+++ js/libraries/gmap.js
@@ -4,11 +4,11 @@
   // initMap() - функция инициализации карты
   function initMap() {
   	// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
-  	var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+  	var centerLatLng = new google.maps.LatLng(1.286036, 103.847851);
   	// Обязательные опции с которыми будет проинициализированна карта
   	var mapOptions = {
   		center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-  		zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
+  		zoom: 14               // Зум по умолчанию. Возможные значения от 0 до 21
   	};
   	// Создаем карту внутри элемента #map
   	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
